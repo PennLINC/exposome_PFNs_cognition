20af2b2beb860dfc8b51da7dd29e3784b2fd9b47
askeller <arielleskeller@gmail.com>
2023-06-27 14:58:09 -0400

    added AIC criterion

--- Step2_RidgeRegressions/ExpFactorPFNs-Cognition/compare_predicted_actual_PFNvsExp_Cognition.m
+++ Step2_RidgeRegressions/ExpFactorPFNs-Cognition/compare_predicted_actual_PFNvsExp_Cognition.m
@@ -112,71 +112,133 @@ p(1,2)
 
 %% Plotting
 
-% load colors
-color_fill = gray(10);
-color_fill_1 = color_fill(1,:);
-color_fill_2 = color_fill(7,:);
+% % load colors
+% color_fill = gray(10);
+% color_fill_1 = color_fill(1,:);
+% color_fill_2 = color_fill(7,:);
+% 
+% figure()
+% scatter(actual_PC1_samp2,pred_PC1_samp2,15,color_fill_2,'Marker','^','MarkerFaceColor',color_fill_2)
+% [r,p,ci,stats]=corrcoef(actual_PC1_samp2,pred_PC1_samp2)
+% hold on
+% scatter(actual_PC1_samp1,pred_PC1_samp1,75,color_fill_1,'Marker','.')
+% [r,p,ci,stats]=corrcoef(actual_PC1_samp1,pred_PC1_samp1)
+% hold on
+% linearFit = polyfit(actual_PC1_samp1,pred_PC1_samp1,1);
+% hline = refline(linearFit);
+% hline.Color=color_fill_1;
+% hline.LineWidth=2;
+% hold on
+% linearFit = polyfit(actual_PC1_samp2,pred_PC1_samp2,1);
+% hline = refline(linearFit);
+% hline.Color=color_fill_2;
+% hline.LineWidth=2;
+% xlabel("Actual Cognitive Performance",'FontSize',16)
+% ylabel("Predicted Cognitive Performance",'FontSize',16)
+% axis([-4 4 -4 4])
+% 
+% 
+% figure()
+% scatter(actual_PC2_samp2,pred_PC2_samp2,15,color_fill_2,'Marker','^','MarkerFaceColor',color_fill_2)
+% [r,p,ci,stats]=corrcoef(actual_PC2_samp2,pred_PC2_samp2)
+% hold on
+% scatter(actual_PC2_samp1,pred_PC2_samp1,75,color_fill_1,'Marker','.')
+% [r,p,ci,stats]=corrcoef(actual_PC2_samp1,pred_PC2_samp1)
+% hold on
+% linearFit = polyfit(actual_PC2_samp1,pred_PC2_samp1,1);
+% hline = refline(linearFit);
+% hline.Color=color_fill_1;
+% hline.LineWidth=2;
+% hold on
+% linearFit = polyfit(actual_PC2_samp2,pred_PC2_samp2,1);
+% hline = refline(linearFit);
+% hline.Color=color_fill_2;
+% hline.LineWidth=2;
+% xlabel("Actual Cognitive Performance",'FontSize',16)
+% ylabel("Predicted Cognitive Performance",'FontSize',16)
+% axis([-4 4 -4 4])
+% 
+% 
+% figure()
+% scatter(actual_PC3_samp2,pred_PC3_samp2,15,color_fill_2,'Marker','^','MarkerFaceColor',color_fill_2)
+% [r,p,ci,stats]=corrcoef(actual_PC3_samp2,pred_PC3_samp2)
+% hold on
+% scatter(actual_PC3_samp1,pred_PC3_samp1,75,color_fill_1,'Marker','.')
+% [r,p,ci,stats]=corrcoef(actual_PC3_samp1,pred_PC3_samp1)
+% hold on
+% linearFit = polyfit(actual_PC3_samp1,pred_PC3_samp1,1);
+% hline = refline(linearFit);
+% hline.Color=color_fill_1;
+% hline.LineWidth=2;
+% hold on
+% linearFit = polyfit(actual_PC3_samp2,pred_PC3_samp2,1);
+% hline = refline(linearFit);
+% hline.Color=color_fill_2;
+% hline.LineWidth=2;
+% xlabel("Actual Cognitive Performance",'FontSize',16)
+% ylabel("Predicted Cognitive Performance",'FontSize',16)
+% axis([-4 4 -4 4])
 
-figure()
-scatter(actual_PC1_samp2,pred_PC1_samp2,15,color_fill_2,'Marker','^','MarkerFaceColor',color_fill_2)
-[r,p,ci,stats]=corrcoef(actual_PC1_samp2,pred_PC1_samp2)
-hold on
-scatter(actual_PC1_samp1,pred_PC1_samp1,75,color_fill_1,'Marker','.')
-[r,p,ci,stats]=corrcoef(actual_PC1_samp1,pred_PC1_samp1)
-hold on
-linearFit = polyfit(actual_PC1_samp1,pred_PC1_samp1,1);
-hline = refline(linearFit);
-hline.Color=color_fill_1;
-hline.LineWidth=2;
-hold on
-linearFit = polyfit(actual_PC1_samp2,pred_PC1_samp2,1);
-hline = refline(linearFit);
-hline.Color=color_fill_2;
-hline.LineWidth=2;
-xlabel("Actual Cognitive Performance",'FontSize',16)
-ylabel("Predicted Cognitive Performance",'FontSize',16)
-axis([-4 4 -4 4])
 
 
-figure()
-scatter(actual_PC2_samp2,pred_PC2_samp2,15,color_fill_2,'Marker','^','MarkerFaceColor',color_fill_2)
-[r,p,ci,stats]=corrcoef(actual_PC2_samp2,pred_PC2_samp2)
-hold on
-scatter(actual_PC2_samp1,pred_PC2_samp1,75,color_fill_1,'Marker','.')
-[r,p,ci,stats]=corrcoef(actual_PC2_samp1,pred_PC2_samp1)
-hold on
-linearFit = polyfit(actual_PC2_samp1,pred_PC2_samp1,1);
-hline = refline(linearFit);
-hline.Color=color_fill_1;
-hline.LineWidth=2;
-hold on
-linearFit = polyfit(actual_PC2_samp2,pred_PC2_samp2,1);
-hline = refline(linearFit);
-hline.Color=color_fill_2;
-hline.LineWidth=2;
-xlabel("Actual Cognitive Performance",'FontSize',16)
-ylabel("Predicted Cognitive Performance",'FontSize',16)
-axis([-4 4 -4 4])
+%% Calculate AIC
+
+% Use the formula
+%    AIC = n * ln(RSS/n) + 2*k
+%        where n = the sample size
+%          and k = the number of parameters in the model 
+
+k=1010004; % 59,412 vertices * 17 networks
+
+% PC1 
+
+n_samp1 = length(actual_PC1_samp1);
+n_samp2 = length(actual_PC1_samp2);
+
+RSS_PC1_samp1_null = sum((actual_PC1_samp1-pred_PC1_samp1_null).^2);
+RSS_PC1_samp2_null = sum((actual_PC1_samp2-pred_PC1_samp2_null).^2);
+
+RSS_PC1_samp1_full = sum((actual_PC1_samp1-pred_PC1_samp1_full).^2);
+RSS_PC1_samp2_full = sum((actual_PC1_samp2-pred_PC1_samp2_full).^2);
+
+AIC_PC1_samp1_null = n_samp1 * log(RSS_PC1_samp1_null/n_samp1) + 2*k
+AIC_PC1_samp2_null = n_samp2 * log(RSS_PC1_samp2_null/n_samp2) + 2*k
+
+AIC_PC1_samp1_full = n_samp1 * log(RSS_PC1_samp1_full/n_samp1) + 2*k
+AIC_PC1_samp2_full = n_samp2 * log(RSS_PC1_samp2_full/n_samp2) + 2*k
 
 
-figure()
-scatter(actual_PC3_samp2,pred_PC3_samp2,15,color_fill_2,'Marker','^','MarkerFaceColor',color_fill_2)
-[r,p,ci,stats]=corrcoef(actual_PC3_samp2,pred_PC3_samp2)
-hold on
-scatter(actual_PC3_samp1,pred_PC3_samp1,75,color_fill_1,'Marker','.')
-[r,p,ci,stats]=corrcoef(actual_PC3_samp1,pred_PC3_samp1)
-hold on
-linearFit = polyfit(actual_PC3_samp1,pred_PC3_samp1,1);
-hline = refline(linearFit);
-hline.Color=color_fill_1;
-hline.LineWidth=2;
-hold on
-linearFit = polyfit(actual_PC3_samp2,pred_PC3_samp2,1);
-hline = refline(linearFit);
-hline.Color=color_fill_2;
-hline.LineWidth=2;
-xlabel("Actual Cognitive Performance",'FontSize',16)
-ylabel("Predicted Cognitive Performance",'FontSize',16)
-axis([-4 4 -4 4])
+% PC2
+
+n_samp1 = length(actual_PC2_samp1);
+n_samp2 = length(actual_PC2_samp2);
+
+RSS_PC2_samp1_null = sum((actual_PC2_samp1-pred_PC2_samp1_null).^2);
+RSS_PC2_samp2_null = sum((actual_PC2_samp2-pred_PC2_samp2_null).^2);
+
+RSS_PC2_samp1_full = sum((actual_PC2_samp1-pred_PC2_samp1_full).^2);
+RSS_PC2_samp2_full = sum((actual_PC2_samp2-pred_PC2_samp2_full).^2);
+
+AIC_PC2_samp1_null = n_samp1 * log(RSS_PC2_samp1_null/n_samp1) + 2*k
+AIC_PC2_samp2_null = n_samp2 * log(RSS_PC2_samp2_null/n_samp2) + 2*k
+
+AIC_PC2_samp1_full = n_samp1 * log(RSS_PC2_samp1_full/n_samp1) + 2*k
+AIC_PC2_samp2_full = n_samp2 * log(RSS_PC2_samp2_full/n_samp2) + 2*k
 
 
+% PC3 
+
+n_samp1 = length(actual_PC3_samp1);
+n_samp2 = length(actual_PC3_samp2);
+
+RSS_PC3_samp1_null = sum((actual_PC3_samp1-pred_PC3_samp1_null).^2);
+RSS_PC3_samp2_null = sum((actual_PC3_samp2-pred_PC3_samp2_null).^2);
+
+RSS_PC3_samp1_full = sum((actual_PC3_samp1-pred_PC3_samp1_full).^2);
+RSS_PC3_samp2_full = sum((actual_PC3_samp2-pred_PC3_samp2_full).^2);
+
+AIC_PC3_samp1_null = n_samp1 * log(RSS_PC3_samp1_null/n_samp1) + 2*k
+AIC_PC3_samp2_null = n_samp2 * log(RSS_PC3_samp2_null/n_samp2) + 2*k
+
+AIC_PC3_samp1_full = n_samp1 * log(RSS_PC3_samp1_full/n_samp1) + 2*k
+AIC_PC3_samp2_full = n_samp2 * log(RSS_PC3_samp2_full/n_samp2) + 2*k
