c5e359013f6ce2dd5c760bb6b0fc15c24cfd7f49
askeller <arielleskeller@gmail.com>
2023-07-06 13:44:05 -0400

    added wrapper scripts

---
+++ Step2_RidgeRegressions/ExpFactorPFNs-T2CogPred/T2CogPred_proc_predict_ExpFactorPFNs.py
@@ -0,0 +1,33 @@
+import sys
+import os
+
+homedir = sys.argv[1]
+tmpdir = sys.argv[2]
+network = sys.argv[3]
+phenotype_name = sys.argv[4]
+
+"""
+this makes the features and targets in tmpdir
+"""
+if phenotype_name == 'PicVocabT2':
+	os.system('python /{0}/T2CogPred_preprocess_ExpFactorPFNs_PicVocab.py {1} {2}'.format(homedir,network,tmpdir))
+elif phenotype_name == 'FlankerT2':
+	os.system('python /{0}/T2CogPred_preprocess_ExpFactorPFNs_Flanker.py {1} {2}'.format(homedir,network,tmpdir))
+elif phenotype_name == 'PictureT2':
+	os.system('python /{0}/T2CogPred_preprocess_ExpFactorPFNs_Picture.py {1} {2}'.format(homedir,network,tmpdir))
+elif phenotype_name == 'PatternT2':
+	os.system('python /{0}/T2CogPred_preprocess_ExpFactorPFNs_Pattern.py {1} {2}'.format(homedir,network,tmpdir))
+elif phenotype_name == 'ReadingT2':
+	os.system('python /{0}/T2CogPred_preprocess_ExpFactorPFNs_Reading.py {1} {2}'.format(homedir,network,tmpdir))
+else:
+	print("Cognitive Task Not Implemented")
+
+working_dir = homedir + '/results_T2CogPred_ExpFactorPFNs/{0}_network/'.format(network)
+feature_path = '/{0}/features_{1}.npy'.format(tmpdir,network)
+phenotype_path = '/{0}/phenotypes_target.csv'.format(tmpdir)
+control_path = '/{0}/phenotypes_control.csv'.format(tmpdir)
+
+"""
+this does the actual ridge regression
+"""
+os.system('python /{0}/predict_matchedsamples_ExpFactor_PFNs.py {1} {2} {3} {4} {5} rel_family_id'.format(homedir,working_dir,feature_path,phenotype_path,phenotype_name,control_path))
